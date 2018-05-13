a14209d23f86dc1a21c15367a0f059096ed53d20
Venkatesh Sreedharan <venkatesh.sreedharan@freshworks.com>
2018-05-13 23:58:08 +0530

    Finding by class name

--- guardian_live.py
+++ guardian_live.py
@@ -6,6 +6,8 @@ import certifi
 from datetime import datetime
 
 
+# def get_team_names():
+
 def getText(node):
 	return node.text.encode('utf-8').decode('ascii','ignore')
 
@@ -21,16 +23,12 @@ def cleanText(text):
 def get_commentary(url):
 	content = urllib2.urlopen(url,cafile=certifi.where()).read()
 	soup = BeautifulSoup(content,"lxml")
-	nav = soup.find_all("div",{"class":"liveblog-navigation__older"})[0].find_all("a")[0].get("href")
-	commentary = map(cleanText, map(getText, soup.find_all("div", {"class": "block-elements"})))
-	print commentary
-	print 'NEXT'
+	nav = soup.find_all(class_ = "liveblog-navigation__older")[0].find_all("a")[0].get("href")
+	commentary = map(cleanText, map(getText, soup.find_all(class_ = "block-elements")))
 	if nav:
 		commentary.extend(get_commentary("https://www.theguardian.com/"+nav))
-	
 	return commentary
 
 url = 'https://www.theguardian.com/football/live/2018/may/10/west-ham-united-v-manchester-united-premier-league-live'
-
 commentary = get_commentary(url)
 
