634d0b21a0c3f0e7185bad1b2c2bf7425954b050
Venkatesh Sreedharan <venkatesh.sreedharan@freshworks.com>
2018-05-16 00:57:59 +0530

    Found teams involved. Created aggregated player commentary. Included stopwords. Formed Wordcloud.

--- guardian_live.py
+++ guardian_live.py
@@ -6,9 +6,29 @@ import certifi
 import pdb
 from datetime import datetime
 import re
+from os import path
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+import random
+from wordcloud import WordCloud, STOPWORDS
+
+def word_cloud(commentary):
+	wordcloud = WordCloud(font_path='/Library/Fonts/Verdana.ttf',
+                      relative_scaling = 1.0,
+                      stopwords = stopwords
+                      ).generate(commentary)
+	plt.imshow(wordcloud)
+	plt.axis("off")
+	plt.show()
+
+def aggregate_commentary(commentary):
+	global player_commentary_agg
+	player_commentary_agg = player_commentary_agg + commentary[0][1]
 
 
-# def get_team_names():
+def get_player_text(commentary):
+	if player in commentary[1]:
+		return True
 
 def getText(node):
 	return node.text.encode('utf-8').decode('ascii','ignore')
@@ -20,6 +40,12 @@ def cleanText(text):
 		return (commentary,text[text.find('mins:')+5:])
 	else:
 		return (commentary,text)
+
+def teams_involved(url):
+	content = urllib2.urlopen(url,cafile=certifi.where()).read()
+	soup = BeautifulSoup(content,"lxml")
+	teams = soup.find_all(class_ = 'block-title')[0].text
+	return teams[teams.find(":")+2:].split(' ')
 
 
 def get_commentary(url):
@@ -40,10 +66,37 @@ def mapping_commentary_to_players(commentary):
 	active_players = [player for player in players if player in commentary[1]]
 	return (commentary,active_players)
 
+
+unwanted_words = ['Reuters','Getty','Photograph']
 url = 'https://www.theguardian.com/football/live/2018/may/10/west-ham-united-v-manchester-united-premier-league-live'
+
+
 commentary = get_commentary(url)
 players = get_player_names(commentary[-2][1])
+
+#Last two values of tuples gives team news. Not necessary.
+commentary = commentary[:-2]
+
+
 mapping = map(mapping_commentary_to_players,commentary)
+
+stopwords_teams = teams_involved(url)
+
+player = 'Pogba'
+player_commentary = filter(get_player_text,mapping)
+
+player_commentary_agg = ''
+map(aggregate_commentary,player_commentary)
+
+stopwords = set(STOPWORDS)
+stopwords.add(player)
+stopwords.add(player+'s')
+stopwords.update(stopwords_teams)
+stopwords.update(unwanted_words)
+
+word_cloud(player_commentary_agg)
+
+
 
 
 
